938c2bde3dbd66c4fe1378590800e82a24ed30aa
Elliott Tulchinsky <tulchies@dukes.jmu.edu>
2024-03-31 22:31:55 -0400

    changes

---
+++ VisionaryVentures/AUTHTableCreationAndPopulation.sql
@@ -0,0 +1,35 @@
+CREATE TABLE HashedCredentials (
+	UserID INT IDENTITY(1,1) PRIMARY KEY,
+	Username NVARCHAR(50),
+	PasswordHash NVARCHAR(255)
+);
+
+INSERT INTO [Lab4].[dbo].[Accounts] (UserType, UserTypeDescription) Values (1, 'Administrative');
+
+INSERT INTO [Lab4].[dbo].[Users]
+           ([AccountID]
+		   ,[FirstName]
+           ,[LastName]
+           ,[EmailAddress]
+           ,[PhoneNumber]
+           ,[StreetAddress]
+           ,[City]
+           ,[State]
+           ,[PostalCode]
+           ,[Country])
+     VALUES
+           (1
+		   ,'Admin'
+		   ,'User'
+		   ,'Admin.User@gmail.com'
+		   ,'703626-0156'
+		   ,'121 Main St'
+		   ,'Harrisonburg'
+		   ,'VA'
+		   ,22801
+		   ,'United States');
+
+INSERT INTO [AUTH].[dbo].[HashedCredentials]
+	(Username,
+	PasswordHash)
+	VALUES ('admin', '1000:Av+wcY5PfhZM7brRKLg7ASbMuowvvB+R:cBmkA0pgUWequQ06sMayq7zeLr8=')
